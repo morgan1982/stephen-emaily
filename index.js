87be467ae7bb882067e01065a74918bed2fcfe6b
entropy <alexandrisanas@gmail.com>
2018-05-25 20:34:08 +0300

    init

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require("express");
+
+
+
 const app = express(); // creates a new application
 
-app.get("/", (req, res) => { // '/' is the route portion of the router
-  // watching for http request with method get
-  res.send({ hi: "there" });
-});
-
-app.get("/konos", (req, res) => {
-  res.send({ name: "konos" });
-});
 
 
-const PORT = process.env.PORT || 5000 // dynamic port binding
-app.listen(PORT); // instracts node to listen for traffic in port 5000
+app.get('/', (req, res) => {
+    res.send({"hello": "hello"});
+})
+
+
+
+const PORT = process.env.PORT || 5000; 
+app.listen(PORT, () => {
+    console.log(`listening on ${PORT}...`);
+}); 
+
+
